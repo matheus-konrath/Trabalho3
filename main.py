ae8510a775f2224b9b06ada3dd8daa1a11654ae7
matheus-konrath <mkonrath96@gmail.com>
2024-06-23 14:00:45 -0300

    ajustes nas tarefas, está fazendo a assinatura

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.backends import default_backend
+
 import subprocess
 import threading
 import time
